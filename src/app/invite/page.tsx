5fffad6211b8316cf2830aeb646f885ebac312df
Mauricio Cardoso <mauricio.cds00@gmail.com>
2025-02-19 20:47:45 -0300

    feat: indications page

--- src/app/invite/page.tsx
+++ src/app/invite/page.tsx
@@ -1,6 +1,6 @@
 import IconButton from "@/components/icon-button";
 import { InputField, InputIcon, InputRoot } from "@/components/input";
-import { Copy, Link } from "lucide-react";
+import { BadgeCheck, Copy, Link, Medal, MousePointerClick } from "lucide-react";
 import Image from "next/image";
 import React from "react";
 import logo from "../../assets/logo.svg";
@@ -39,10 +39,45 @@ const Invite = () => {
 
             <InputField readOnly defaultValue="https://www.github.com/" />
 
-            <IconButton className="ml-2 ">
+            <IconButton className="-mr-2">
               <Copy className="size-5" />
             </IconButton>
           </InputRoot>
+
+          <div className="grid md:grid-cols-3 gap-3">
+            <div className="relative bg-gray-700 border border-gray-600 px-4 py-7 flex flex-col items-center justify-center gap-1 rounded-xl">
+              <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
+                1042
+              </span>
+              <span className="text-sm text-gray-300 leading-none text-center">
+                Acessos ao link
+              </span>
+
+              <MousePointerClick className="size-5 text-purple absolute top-3 left-3" />
+            </div>
+
+            <div className="relative bg-gray-700 border border-gray-600 px-4 py-7 flex flex-col items-center justify-center gap-1 rounded-xl">
+              <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
+                875
+              </span>
+              <span className="text-sm text-gray-300 leading-none text-center">
+                Inscrições feitas
+              </span>
+
+              <BadgeCheck className="size-5 text-purple absolute top-3 left-3" />
+            </div>
+
+            <div className="relative bg-gray-700 border border-gray-600 px-4 py-7 flex flex-col items-center justify-center gap-1 rounded-xl">
+              <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
+                3º
+              </span>
+              <span className="text-sm text-gray-300 leading-none text-center">
+                Lugar no ranking
+              </span>
+
+              <Medal className="size-5 text-purple absolute top-3 left-3" />
+            </div>
+          </div>
         </div>
       </div>
     </div>
