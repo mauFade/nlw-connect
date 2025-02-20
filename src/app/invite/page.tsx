9a78042afaa9aad3defcca0c03c23cfe4bfae474
Mauricio Cardoso <mauricio.cds00@gmail.com>
2025-02-19 21:10:41 -0300

    feat: finish ranking

--- src/app/invite/page.tsx
+++ src/app/invite/page.tsx
@@ -3,7 +3,10 @@ import { InputField, InputIcon, InputRoot } from "@/components/input";
 import { BadgeCheck, Copy, Link, Medal, MousePointerClick } from "lucide-react";
 import Image from "next/image";
 import React from "react";
+import bronzeMedal from "../../assets/bronze-medal.svg";
+import goldMedal from "../../assets/gold-medal.svg";
 import logo from "../../assets/logo.svg";
+import silverMedal from "../../assets/silver-medal.svg";
 
 const Invite = () => {
   return (
@@ -77,6 +80,62 @@ const Invite = () => {
 
               <Medal className="size-5 text-purple absolute top-3 left-3" />
             </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="w-full max-w-[440px] space-y-5">
+        <h2 className="text-gray-200 text-xl font-heading font-semibold leading-none">
+          Ranking de indicações
+        </h2>
+
+        <div className="space-y-4">
+          <div className="relative rounded-xl bg-gray-700 border border-gray-600 p-6 flex flex-col justify-center gap-3">
+            <span className="text-sm text-gray-300 leading-none">
+              <span className="font-semibold">1º</span> | John Doe
+            </span>
+
+            <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
+              1030
+            </span>
+
+            <Image
+              src={goldMedal}
+              alt="gold-medal"
+              className="absolute top-0 right-8"
+            />
+          </div>
+
+          <div className="relative rounded-xl bg-gray-700 border border-gray-600 p-6 flex flex-col justify-center gap-3">
+            <span className="text-sm text-gray-300 leading-none">
+              <span className="font-semibold">2º</span> | Peter Parker
+            </span>
+
+            <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
+              998
+            </span>
+
+            <Image
+              src={silverMedal}
+              alt="silver-medal"
+              className="absolute top-0 right-8"
+            />
+          </div>
+
+          <div className="relative rounded-xl bg-gray-700 border border-gray-600 p-6 flex flex-col justify-center gap-3">
+            <span className="text-sm text-gray-300 leading-none">
+              <span className="font-semibold">3º</span> | Wade Wilson
+            </span>
+
+            <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
+              781
+            </span>
+
+            <Image
+              src={bronzeMedal}
+              alt="bronze-medal"
+              className="absolute top-0 right-8"
+            />
           </div>
         </div>
       </div>
